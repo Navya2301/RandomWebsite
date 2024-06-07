f49cdbd21a73754955dfc50f0e1afe3613fcf899
Navya2301 <nr9n8@mst.edu>
2024-06-07 14:20:52 -0400

    added images component in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
   trailingSlash: true,
   // For assetPrefix, change it to your GitHub Pages URL if your project is under a repository
   assetPrefix: process.env.NODE_ENV === 'production' ? '/RandomWebsite/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
