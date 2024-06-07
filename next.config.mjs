3b1ddfdfcbf50be32c04a59627e6b3eb0466d006
Navya2301 <nr9n8@mst.edu>
2024-06-07 13:41:13 -0400

    Setup Next.js project for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  // For assetPrefix, change it to your GitHub Pages URL if your project is under a repository
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/navya2301/' : '',
 };
 
 export default nextConfig;
