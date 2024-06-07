e2d5cd8192db4d58f6a81ed56e79f78a375d8ee7
Navya2301 <nr9n8@mst.edu>
2024-06-07 14:01:06 -0400

    Updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   // For assetPrefix, change it to your GitHub Pages URL if your project is under a repository
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/navya2301/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/RandomWebsite/' : '',
 };
 
 export default nextConfig;
