400605654748d0730ee21e204cd77c30d7999f02
Navya2301 <nr9n8@mst.edu>
2024-06-07 13:54:24 -0400

    Removed Nanimation.tsx

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
         <div>
             <h1 className='text-[35px] md:text-[50px] text-white font-bold'>
-                HI I'M 
+                HI I am 
                 <span className='text-yellow-400'> NAVYA RACHA!</span>
             </h1>
       
