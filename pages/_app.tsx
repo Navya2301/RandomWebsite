63d48cc28e9791b9377207de6e865b746d7ce335
Navya2301 <nr9n8@mst.edu>
2024-06-07 23:19:38 -0400

    changes _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import About from "@/Components/About";
+import Blog from "@/Components/Blog";
+import ClientReview from "@/Components/ClientReview";
+import Services from "@/Components/Services";
+import Skils from "@/Components/Skils";
+import Projects from "@/Components/Projects";
+import Testimonials from "@/Components/Testimonials";
+import Footer from "@/Components/Footer";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <> 
+    <About/>
+      {/* Services */}
+    <Services/>
+      {/* Skills */}
+    <Skils/>
+      {/* Projects */}
+    <Projects/>
+      {/* Reviews */}
+    <Testimonials/>
+      {/* BLOG */}
+    <Blog/>
+      {/* Footer */}
+  
+    <Component {...pageProps} />
+    <Footer/>
+    </>
+);
 }
